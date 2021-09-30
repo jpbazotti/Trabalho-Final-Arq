c775df5e2dc128d9545afe9668cfd0a2afa79743
jpbazotti <62719223+jpbazotti@users.noreply.github.com>
2021-09-30 17:56:01 -0300

    manually wrote starting dir

--- fileSystemCreator.cpp
+++ fileSystemCreator.cpp
@@ -5,16 +5,19 @@ void createFile(FileSystem fs,string filename){
     FILE *file;
     int8 full =255;
     int8 empty =0;
+    int8 eof = 28;
     if((file = fopen("main.bin","wb"))){
         fwrite(&fs,sizeof(FileSystem),1,file);
         for(int i=0;i<pow(2,8);i++){
-             fwrite(&empty,sizeof(unsigned char),1,file);
+             fwrite(&empty,sizeof(int8),1,file);
         }
         for(int i=0;i<pow(2,23);i++){
-             fwrite(&empty,sizeof(unsigned char),1,file);
+             fwrite(&empty,sizeof(int8),1,file);
         }
         fseek(file,fs.indexStart,SEEK_SET);
-        fwrite(&full,sizeof(unsigned char),1,file);
+        fwrite(&full,sizeof(int8),1,file);
+        fseek(file,fs.indexStart+pow(2,fs.indexSize),SEEK_SET);
+        fwrite(&eof,sizeof(int8),1,file);
         fclose(file);
     }
 }
