6fb9c6ceaec9efd28b54f134f23ed92b61ad4764
jpbazotti <62719223+jpbazotti@users.noreply.github.com>
2021-10-02 15:40:40 -0300

    create file

--- clusterManagement.h
+++ clusterManagement.h
@@ -4,22 +4,5 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-int8 findAvailableCluster(FILE *file,int8 indexSize,int8 clusterSize){
-    fseek(file,4,SEEK_SET);
-    int8 current;
-    int8 full =255;
-    int8 eof = 28;
-    for(int i =0;i<pow(2,indexSize);i++){
-        fread(&current,sizeof(int8),1,file);
-        if(current==0){
-            int offSet=4+pow(2,indexSize)+ i*pow(2,clusterSize);
-            fseek(file,-1,SEEK_CUR);
-            fwrite(&full,sizeof(int8),1,file);
-            fseek(file,offSet,SEEK_SET);
-            fwrite(&eof,sizeof(int8),1,file);
-            return i;
-        }
-    }
-    return -1;
-}
+int8 findAvailableCluster(FILE *file, int8 indexSize, int8 clusterSize);
 #endif
--- directory.cpp
+++ directory.cpp
@@ -1,34 +1,39 @@
-#include "directory.h""
+#include "directory.h"
 
-bool DIR(FILE *dir){
-  while(1){
-    char *cache;
-    fread(&cache, sizeof(int8), 1, dir);
-    if(cache[0] == 28){
-      break;
+bool DIR(FILE *dir)
+{
+    while (1)
+    {
+        char *cache;
+        fread(&cache, sizeof(int8), 1, dir);
+        if (cache[0] == 28)
+        {
+            break;
+        }
+        fseek(dir, 0, SEEK_CUR - 1);
+        fgets(&cache, sizeof(int8), 9, dir);
+        cout << cache << ".";
+        fgets(&cache, sizeof(int8), 4, dir);
+        cout << cache << "\n";
+        fseek(dir, 1, SEEK_CUR);
     }
-    fseek(dir, 0, SEEK_CUR-1);
-    fgets(&cache, sizeof(int8), 9, dir);
-    cout << cache << ".";
-    fgets(&cache, sizeof(int8), 4, dir);
-    cout << cache << "\n";
-    fseek(dir, 1, SEEK_CUR);
-  }
 }
 
-void createDir(FILE *file, int8 curDir,char dirName[8],int8 clusterSize, int8 indexSize){
+void createDir(FILE *file, int8 curDir, char dirName[9], int8 clusterSize, int8 indexSize)
+{
     int8 eof = 28;
-    int dirOffset= 4 + pow(2,indexSize) + curDir * pow(2,clusterSize); 
+    int dirOffset = 4 + pow(2, indexSize) + curDir * pow(2, clusterSize);
     dirEntry newEntry;
-    strcpy(newEntry.name,dirName);
-    strcpy(newEntry.extension,"dir");
-    newEntry.startCluster=findAvailableCluster(file,indexSize,clusterSize);
-    fseek(file,dirOffset,SEEK_SET);
+    strcpy(newEntry.name, dirName);
+    strcpy(newEntry.extension, "dir");
+    newEntry.startCluster = findAvailableCluster(file, indexSize, clusterSize);
+    fseek(file, dirOffset, SEEK_SET);
     int8 current;
-    do{
-        fread(&current,sizeof(int8),1,file);
-    }while(current != 28);
-    fseek(file,-1,SEEK_CUR);
-    fwrite(&newEntry,sizeof(dirEntry),1,file);
-    fwrite(&eof,sizeof(int8),1,file);
+    do
+    {
+        fread(&current, sizeof(int8), 1, file);
+    } while (current != 28);
+    fseek(file, -1, SEEK_CUR);
+    fwrite(&newEntry, sizeof(dirEntry), 1, file);
+    fwrite(&eof, sizeof(int8), 1, file);
 }
