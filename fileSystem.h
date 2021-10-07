c7d1f21e79bc5d867b83ef735d5f76d59be9751e
Eu não ligo kk <izaiassaturninodelimaneto@hotmail.com>
2021-10-07 17:53:11 -0300

    MOVE command

--- fileSystem.h
+++ fileSystem.h
@@ -22,11 +22,14 @@ bool gotoCluster(FILE *file, int8 clusterIndex, FileSystem fs);
 //Entra na pasta com o nome que foi passado, se ela existir no diretorio atual. Retorna o cluster atual no ponteiro clusterIndex
 bool gotoDir(char *name, FILE *dir, FileSystem fs, int8 *clusterIndex);
 
-//Faz o comando cd e retorna o cluster atual no ponteiro clusterIndex
+//Faz o comando CD e retorna o cluster atual no ponteiro clusterIndex
 bool CD(char *names, FILE *dir, FileSystem fs, int8 *clusterIndex);
 
 //retorna o nome do arquivo
 char *getFileName(dirEntry Entry);
+
+//retorna se o diretorio esta vazio ou nao
+bool isDirEmpty(FILE *file);
 
 //faz o commando DIR em um cluster somente
 void DIR(FILE *dir);
@@ -37,10 +40,11 @@ bool validPath(char *path);
 //Quebra a string para encontrar o nome do arquivo
 char *breakePath(char *path);
 
+//faz o comando RM
 bool RM(char *path, FILE *file, FileSystem fs, int8 *clusterIndex);
 
-//faz o comando RM
-bool remove(char *name);
+//faz o comando MOVE
+bool MOVE(char *path1, char *path2, FILE *file, FileSystem fs, int8 *clusterIndex);
 
 //cria novo diretorio em um diretorio, no diretorio procura por um espaco vazio ou o final do diretorio se nao ha um disponivel
 void createDir(FILE *file, int8 curDir, const char dirName[9], int8 clusterSize, int8 indexSize);
